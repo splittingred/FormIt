6c15acf823d98fe28ba5862838be8a7312659fa1
Shaun McCormick <shaun@modx.com>
2010-11-02 09:25:12 -0500

    - Added FR translation

---
+++ core/components/formit/lexicon/fr/default.inc.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * FormIt
+ *
+ * Copyright 2009-2010 by Shaun McCormick <shaun@modx.com>
+ *
+ * FormIt is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * FormIt is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * FormIt; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package formit
+ */
+/**
+ * Default Lexicon Topic
+ *
+ * @package formit
+ * @subpackge lexicon
+ */
+$_lang['formit.contains'] = 'Votre valeur doit contenir la phrase "[[+value]]".';
+$_lang['formit.email_invalid'] = 'Entrez une adresse courriel valide.';
+$_lang['formit.email_invalid_domain'] = 'Votre adresse courriel n\'a pas un nom de domaine valide.';
+$_lang['formit.email_no_recipient'] = 'SpÈcifiez un ou plusieurs destinataires du courriel.';
+$_lang['formit.email_not_sent'] = 'Une erreur s\'est produite lors de l\'envoi du courriel.';
+$_lang['formit.email_tpl_nf'] = 'SpÈcifiez un modËle de courriel.';
+$_lang['formit.field_not_empty'] = 'Ce champ doit Ítre vide.';
+$_lang['formit.field_required'] = 'Ce champ est obligatoire.';
+$_lang['formit.max_length'] = 'Ce champ ne peut pas Ítre plus long que [[+length]] caractËres.';
+$_lang['formit.max_value'] = 'Ce champ ne peut pas Ítre plus grand que [[+value]].';
+$_lang['formit.min_length'] = 'Ce champ doit contenir au moins [[+length]] caractËres.';
+$_lang['formit.min_value'] = 'Ce champ ne peut pas Ítre plus petit que [[+value]].';
+$_lang['formit.not_date'] = 'Ce champ doit contenir une date valide.';
+$_lang['formit.not_lowercase'] = 'Ce champ doit Ítre en minuscule.';
+$_lang['formit.not_number'] = 'Ce champ doit Ítre un nombre valide.';
+$_lang['formit.not_uppercase'] = 'Ce champ doit Ítre en majuscules.';
+$_lang['formit.password_dont_match'] = 'Les mots de passe ne correspondent pas.';
+$_lang['formit.password_not_confirmed'] = 'Confirmez le mot de passe.';
+$_lang['formit.range_invalid'] = 'Valeurs invalides.';
+$_lang['formit.range'] = 'La valeur doit Ítre comprise entre [[+min]] et [[+max]].';
+$_lang['formit.recaptcha_err_load'] = 'Le service ReCaptcha n\'a pas pu charger la classe.';
+$_lang['formit.spam_blocked'] = 'Votre inscription a ÈtÈ bloquÈe par un filtre anti-spam: ';
+$_lang['formit.spam_marked'] = ' - marquÈs comme spam.';
+$_lang['formit.username_taken'] = 'Ce nom d\'utilisateur existe dÈj‡, choisissez en un autre.';
