523321568e3e91548cd524f09d0253a7b2d45a3c
Alexandr Voronkin <admin@elasticweb.ru>
2010-12-07 17:37:36 +0300

    Updated russian translation

--- core/components/formit/lexicon/ru/recaptcha.inc.php
+++ core/components/formit/lexicon/ru/recaptcha.inc.php
@@ -1,30 +1,4 @@
 <?php
-/**
- * FormIt
- *
- * Copyright 2009-2010 by Shaun McCormick <shaun@modx.com>
- *
- * FormIt is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option) any
- * later version.
- *
- * FormIt is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * FormIt; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
- *
- * @package formit
- */
-/**
- * reCaptcha Lexicon Topic
- *
- * @package formit
- * @subpackge lexicon
- */
 $_lang['recaptcha.empty_answer'] = 'Слова введены неправильно. Пожалуйста, проверьте ваш ответ и повторите попытку.';
 $_lang['recaptcha.incorrect'] = 'Код капчи был введён неправильно. Вернитесь назад и попробуйте снова. [[+error]]';
 $_lang['recaptcha.mailhide_no_mcrypt'] = 'Для использования reCAPTCHA Mailhide у вас должен быть установлен PHP модуль mcrypt.';
