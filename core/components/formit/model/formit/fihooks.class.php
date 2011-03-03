568ea0cde1523809175167e93e26fa16a3ae4cb3
Shaun McCormick <shaun@modx.com>
2011-03-03 07:40:43 -0600

    Fix issue with math hook where error placeholders were incorrect

--- core/components/formit/model/formit/fihooks.class.php
+++ core/components/formit/model/formit/fihooks.class.php
@@ -475,14 +475,14 @@ class fiHooks {
      * @return boolean True if email was successfully sent.
      */
     public function math(array $fields = array()) {
+        $mathField = $this->modx->getOption('mathField',$this->formit->config,'math');
+        if (empty($fields[$mathField])) { $this->errors[$mathField] = $this->modx->lexicon('formit.math_field_nf',array('field' => $mathField)); return false; }
         $op1Field = $this->modx->getOption('mathOp1Field',$this->formit->config,'op1');
         if (empty($fields[$op1Field])) { $this->errors[$mathField] = $this->modx->lexicon('formit.math_field_nf',array('field' => $op1Field)); return false; }
         $op2Field = $this->modx->getOption('mathOp2Field',$this->formit->config,'op2');
         if (empty($fields[$op2Field])) { $this->errors[$mathField] = $this->modx->lexicon('formit.math_field_nf',array('field' => $op2Field)); return false; }
         $operatorField = $this->modx->getOption('mathOperatorField',$this->formit->config,'operator');
         if (empty($fields[$operatorField])) { $this->errors[$mathField] = $this->modx->lexicon('formit.math_field_nf',array('field' => $operatorField)); return false; }
-        $mathField = $this->modx->getOption('mathField',$this->formit->config,'math');
-        if (empty($fields[$mathField])) { $this->errors[$mathField] = $this->modx->lexicon('formit.math_field_nf',array('field' => $mathField)); return false; }
 
         $answer = false;
         $op1 = (int)$fields[$op1Field];
