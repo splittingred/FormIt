6f0756d35ca7e5ee385485d848116d2703220706
Shaun McCormick <shaun@modx.com>
2011-06-22 12:32:05 -0500

    [#4993] Add Italian translation and fix recaptcha links

--- core/components/formit/lexicon/cs/recaptcha.inc.php
+++ core/components/formit/lexicon/cs/recaptcha.inc.php
@@ -31,7 +31,7 @@
 $_lang['recaptcha.empty_answer'] = 'Nesprávná captcha slova. Zkontrolujte svou odpověď a zkuste to znovu.';
 $_lang['recaptcha.incorrect'] = 'reCAPTCHA nebyla zadána správně. Jděte zpět a zkuste to znovu. [[+error]]';
 $_lang['recaptcha.mailhide_no_mcrypt'] = 'Pro využívání reCAPTCHA Mailhide musíte mít nainstalovaný PHP modul "mcrypt".';
-$_lang['recaptcha.mailhide_no_api_key'] = 'Pro využívání reCAPTCHA Mailhide se musíte zaregistrovat a tak získat veřejný a soukromý klíč, můžete tak učinit na adrese <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
-$_lang['recaptcha.no_api_key'] = 'Pro využívání reCAPTCHA musíte získat klíč API na adrese <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
+$_lang['recaptcha.mailhide_no_api_key'] = 'Pro využívání reCAPTCHA Mailhide se musíte zaregistrovat a tak získat veřejný a soukromý klíč, můžete tak učinit na adrese <a href="http://www.google.com/recaptcha">http://www.google.com/recaptcha</a>';
+$_lang['recaptcha.no_api_key'] = 'Pro využívání reCAPTCHA musíte získat klíč API na adrese <a href="http://www.google.com/recaptcha">http://www.google.com/recaptcha</a>';
 $_lang['recaptcha.no_remote_ip'] = 'Z bezpečnostních důvodů, musíte zadat vzdálenou IP do reCAPTCHA';
 
