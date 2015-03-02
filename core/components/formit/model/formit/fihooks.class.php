8e69d3b24207328cf9964ddb4b149e98cc3cc72a
Wieger Sloot <bumme@Wiegers-iMac.local>
2015-03-02 13:19:38 +0100

    Added HTML5 multiple file upload support
    Fix issue #16

--- core/components/formit/model/formit/fihooks.class.php
+++ core/components/formit/model/formit/fihooks.class.php
@@ -455,15 +455,29 @@ class fiHooks {
         /* handle file fields */
         $attachmentIndex = 0;
         foreach ($origFields as $k => $v) {
-            if (is_array($v) && !empty($v['tmp_name']) && isset($v['error']) && $v['error'] == UPLOAD_ERR_OK) {
-                if (empty($v['name'])) {
-                    $v['name'] = 'attachment'.$attachmentIndex;
+            if (is_array($v) && !empty($v['tmp_name'])) {
+                if(count($v['name']) > 1){
+                    for($i=0;$i<count($v['name']);++$i){
+                        if(isset($v['error'][$i]) && $v['error'][$i] == UPLOAD_ERR_OK){
+                            if (empty($v['name'][$i])) {
+                                $v['name'][$i] = 'attachment'.$attachmentIndex;
+                            }                    
+                            $this->modx->mail->mailer->addAttachment($v['tmp_name'][$i],$v['name'][$i],'base64',!empty($v['type'][$i]) ? $v['type'][$i] : 'application/octet-stream');
+                            $attachmentIndex++;
+                        }
+                    }
+                }else{
+                    if(isset($v['error']) && $v['error'] == UPLOAD_ERR_OK){
+                        if (empty($v['name'])) {
+                            $v['name'] = 'attachment'.$attachmentIndex;
+                        }                    
+                        $this->modx->mail->mailer->addAttachment($v['tmp_name'],$v['name'],'base64',!empty($v['type']) ? $v['type'] : 'application/octet-stream');
+                        $attachmentIndex++;
+                    }
                 }
-                $this->modx->mail->mailer->addAttachment($v['tmp_name'],$v['name'],'base64',!empty($v['type']) ? $v['type'] : 'application/octet-stream');
-                $attachmentIndex++;
             }
         }
-
+        
         /* add to: with support for multiple addresses */
         $emailTo = explode(',',$emailTo);
         $emailToName = explode(',',$emailToName);
