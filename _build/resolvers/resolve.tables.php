ce5cd865f41a6dcdb225672fa434a03f5d5641f8
Sepia River <info@sepiariver.com>
2016-06-17 15:51:03 -0500

    1st pass update resolver change

--- _build/resolvers/resolve.tables.php
+++ _build/resolvers/resolve.tables.php
@@ -18,15 +18,41 @@ if ($object->xpdo) {
 
             break;
         case xPDOTransport::ACTION_UPGRADE:
+            /** @var modX $modx */
             $modx =& $object->xpdo;
+            // http://forums.modx.com/thread/88734/package-version-check#dis-post-489104
+            $c = $modx->newQuery('transport.modTransportPackage');
+            $c->where(array(
+                'workspace' => 1,
+                "(SELECT
+                    `signature`
+                  FROM {$modx->getTableName('modTransportPackage')} AS `latestPackage`
+                  WHERE `latestPackage`.`package_name` = `modTransportPackage`.`package_name`
+                  ORDER BY
+                     `latestPackage`.`version_major` DESC,
+                     `latestPackage`.`version_minor` DESC,
+                     `latestPackage`.`version_patch` DESC,
+                     IF(`release` = '' OR `release` = 'ga' OR `release` = 'pl','z',`release`) DESC,
+                     `latestPackage`.`release_index` DESC
+                  LIMIT 1,1) = `modTransportPackage`.`signature`",
+            ));
+            $c->where(array(
+                'modTransportPackage.package_name' => 'formit',
+                'installed:IS NOT' => null
+            ));
+            /** @var modTransportPackage $oldPackage */
+            $oldPackage = $modx->getObject('transport.modTransportPackage', $c);
             $modelPath = $modx->getOption('formit.core_path',null,$modx->getOption('core_path').'components/formit/').'model/';
             $modx->addPackage('formit',$modelPath);
-
-            $manager = $modx->getManager();
-            $manager->createObjectContainer('FormItForm'); // Dirty fix for upgrading from an older FormIt 
-            $manager->addField('FormItForm', 'encrypted');
-
-
+            
+            if ($oldPackage && $oldPackage->compareVersion('2.2.8-pl', '>')) {
+                $manager = $modx->getManager();
+                $manager->addField('FormItForm', 'encrypted');
+            }
+            if ($oldPackage && $oldPackage->compareVersion('2.2.10-pl', '>')) {
+                $manager = $modx->getManager();
+                $manager->addField('FormItForm', 'hash');
+            }
             break;
     }
 }
