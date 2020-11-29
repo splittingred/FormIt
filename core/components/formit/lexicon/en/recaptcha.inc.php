11f85da3a5a18373e4bc6decb7742db20bf60dd0
rolecs <rolecs@users.noreply.github.com>
2020-11-29 17:40:24 +0200

    Added Ukrainian lexicon. Some changes in the Russian lexicon and minor changes in the English lexicon.

--- core/components/formit/lexicon/en/recaptcha.inc.php
+++ core/components/formit/lexicon/en/recaptcha.inc.php
@@ -3,12 +3,12 @@
 /**
  * FormIt
  *
- * Copyright 2019 by Sterc <modx@sterc.nl>
+ * Copyright 2020 by Sterc <modx@sterc.nl>
  */
 
 $_lang['recaptcha.empty_answer']                                = 'Incorrect captcha words. Please check your answer and try again.';
 $_lang['recaptcha.incorrect']                                   = 'The reCAPTCHA wasn\'t entered correctly. Go back and try it again. [[+error]]';
 $_lang['recaptcha.mailhide_no_mcrypt']                          = 'To use reCAPTCHA Mailhide, you need to have the mcrypt php module installed.';
-$_lang['recaptcha.mailhide_no_api_key']                         = 'To use reCAPTCHA Mailhide, you have to sign up for a public and private key, you can do so at <a href="http://www.google.com/recaptcha">http://www.google.com/recaptcha</a>';
-$_lang['recaptcha.no_api_key']                                  = 'To use reCAPTCHA you must get an API key from <a href="http://www.google.com/recaptcha">http://www.google.com/recaptcha</a>';
-$_lang['recaptcha.no_remote_ip']                                = 'For security reasons, you must pass the remote ip to reCAPTCHA';
+$_lang['recaptcha.mailhide_no_api_key']                         = 'To use reCAPTCHA Mailhide, you have to sign up for a public and private key, you can do so at <a href="https://www.google.com/recaptcha" target="_blank">https://www.google.com/recaptcha</a>';
+$_lang['recaptcha.no_api_key']                                  = 'To use reCAPTCHA you must get an API key from <a href="https://www.google.com/recaptcha" target="_blank">https://www.google.com/recaptcha</a>';
+$_lang['recaptcha.no_remote_ip']                                = 'For security reasons, you must pass the remote IP to reCAPTCHA';
---
+++ core/components/formit/lexicon/uk/recaptcha.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * FormIt
+ *
+ * Copyright 2020 by Sterc <modx@sterc.nl>
+ */
+
+$_lang['recaptcha.empty_answer']                                = 'Слова введені неправильно. Будь ласка, перевірте вашу відповідь і спробуйте ще раз.';
+$_lang['recaptcha.incorrect']                                   = 'Слова з картинки введені неправильно. Будь ласка, спробуйте ще раз. [[+error]]';
+$_lang['recaptcha.mailhide_no_mcrypt']                          = 'Для використання reCAPTCHA Mailhide у вас повинен бути встановлений PHP модуль mcrypt.';
+$_lang['recaptcha.mailhide_no_api_key']                         = 'Для використання reCAPTCHA Mailhide, вам треба отримати публічний і приватний ключі, це можна зробити на <a href="https://www.google.com/recaptcha" target="_blank">https://www.google.com/recaptcha</a>';
+$_lang['recaptcha.no_api_key']                                  = 'Для використання reCAPTCHA вам треба отримати API ключ на <a href="https://www.google.com/recaptcha" target="_blank">https://www.google.com/recaptcha</a>';
+$_lang['recaptcha.no_remote_ip']                                = 'З міркувань безпеки, ви повинні передати віддалений IP в reCAPTCHA';
