dd9b194f19a7fb744c1af52af5533377f9121536
Mark Hamstra <hello@markhamstra.com>
2021-01-20 22:28:50 +0100

    Update autoloader, based on Composer 1.10.19
    Signed-off-by: Mark Hamstra <hello@markhamstra.com>

--- core/components/formit/vendor/composer/autoload_real.php
+++ core/components/formit/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitf04f5acd9d2ed29ec2c783dd1c99d56e
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
